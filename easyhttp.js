5d61d2ce5f80700959afc75aa43f137a2541a757
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-01-03 21:25:54 +0000

    Initial codes

---
+++ easyhttp.js
@@ -0,0 +1,58 @@
+class EasyHTTP {
+  // Make an http get request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  // Make an http post request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  // Make an http put request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  // Make an http delete request
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then(() => resolve("Resource Deleted..."))
+        .catch((err) => reject(err));
+    });
+  }
+}
